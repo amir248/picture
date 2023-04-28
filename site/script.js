8bd91475529f85fe2c9f5d5dfca607e29d3570df
chikchicly <chikchicly@gmail.com>
2023-04-28 19:33:25 +0700

    oK

--- site/script.js
+++ site/script.js
@@ -1,6 +1,12 @@
-document.querySelector('img').setAttribute('src','site/DSC_7533.JPG');
-
+window.addEventListener('DOMContentLoaded',()=>{
+  document.querySelector('img').setAttribute('src',randomImage());
+});
+function randomImage(){
+  let random=Math.floor(Math.random()*images.length);
+  return images[random];
+}
 let images=['site/DSC_7533.JPG','site/DSC_7536.JPG','site/DSC_7537.JPG','site/DSC_7538.JPG','site/DSC_7539.JPG','site/DSC_7540.JPG','site/function-return-forever.JPG'];
+
 let count=+0;
 document.querySelector('img').addEventListener('click',()=>{
   count++;
