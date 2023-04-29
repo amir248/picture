a092c175072321e52447a6c3e041e1467f0c6ab9
chikchicly <chikchicly@gmail.com>
2023-04-29 07:44:25 +0700

    newPhoto

--- site/script.js
+++ site/script.js
@@ -5,7 +5,7 @@ function randomImage(){
   let random=Math.floor(Math.random()*images.length);
   return images[random];
 }
-let images=['site/DSC_7533.JPG','site/DSC_7536.JPG','site/DSC_7537.JPG','site/DSC_7538.JPG','site/DSC_7539.JPG','site/DSC_7540.JPG','site/function-return-forever.JPG'];
+let images=['site/DSC_7533.JPG','site/DSC_7536.JPG','site/DSC_7537.JPG','site/DSC_7538.JPG','site/DSC_7539.JPG','site/DSC_7540.JPG','site/function-return-forever.JPG','site/DSC_7555.JPG'];
 
 let count=+0;
 document.querySelector('img').addEventListener('click',()=>{
@@ -19,3 +19,7 @@ document.querySelector('img').addEventListener('click',()=>{
     count=0;
   }
 });
+let span=document.createElement('span');
+span.innerHTML=`click flipping <img src='site/click-flipping.png' alt='галерея на яваскритп с перелистванием по клику' style='width:35px'>`;
+span.className='lol';
+document.querySelector('main').append(span);
