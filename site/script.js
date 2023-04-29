4a2a53c37ea5be41de539784fe7be43cd9ec9c4d
chikchicly <chikchicly@gmail.com>
2023-04-29 16:27:48 +0700

    eng

--- site/script.js
+++ site/script.js
@@ -57,20 +57,22 @@ let divTwo=document.createElement('div');
 divTwo.className='toggler-knob';
 document.querySelector('.toggler-slider').append(divTwo);
 
-document.querySelector('input[type=checkbox]').addEventListener('click',()=>{
+document.querySelector('input[type=checkbox]').addEventListener('click',changes);
+function changes(){
   if(document.querySelector('input[type=checkbox]').checked==true){
     text.innerHTML='Super brilliant photo gallery with flipping by clicking on the photo. Tattoo gallery can be embedded into any site and it will work wherever you can change page styles and embed JS("Java Script" ^_^)';
     eng.innerHTML='< - Rus';
     styleCheckbox.innerHTML='Checkbox styles were taken from here <a href="https://alvarotrigo.com/blog/css-checkbox-styles/">styleCheckbox</a>';
     eng.style.cssText=`border:3px solid green;width:100%;background:rgba(0,255,0,0.3);border-radius:7px;color:grey;text-shadow:black 1px 1px;`
+    count=1;
   }else{
     text.innerHTML='Супер блестящая фото галерея с перелистыванием по клику на фотографю. Татую галерею можно встроить в любой сайт и она будет работать везде где можно менять стили страницы и встраивать JS("Джава Скрипт" ^_^)';
     eng.innerHTML='< - Eng';
     styleCheckbox.innerHTML='Стили для чекбоква были взфты отсюда <a href="https://alvarotrigo.com/blog/css-checkbox-styles/">styleCheckbox</a>';
     eng.style.cssText=`border:3px solid grey;width:100%;background:rgba(5,5,5,0.3);border-radius:7px;`
+    counTt=0;
   }
-});
-
+}
 let eng=document.createElement('span');
 eng.style.cssText=`border:3px solid grey;width:100%;background:rgba(5,5,5,0.3);border-radius:7px;color:red;text-shadow:black 1px 1px;`
 eng.innerHTML='< - Eng';
@@ -80,3 +82,24 @@ let styleCheckbox=document.createElement('p');
 styleCheckbox.innerHTML='Стили для чекбоква были взфты отсюда <a href="https://alvarotrigo.com/blog/css-checkbox-styles/">styleCheckbox</a>';
 styleCheckbox.style.cssText='margin:1%; margin-top:3%;'
 document.querySelector('main').append(styleCheckbox);
+let counTt=+0;
+function changesTwo(){
+  counTt++;
+  if(counTt==1){
+    document.querySelector('input[type=checkbox]').checked=true;
+    text.innerHTML='Super brilliant photo gallery with flipping by clicking on the photo. Tattoo gallery can be embedded into any site and it will work wherever you can change page styles and embed JS("Java Script" ^_^)';
+    eng.innerHTML='< - Rus';
+    styleCheckbox.innerHTML='Checkbox styles were taken from here <a href="https://alvarotrigo.com/blog/css-checkbox-styles/">styleCheckbox</a>';
+    eng.style.cssText=`border:3px solid green;width:100%;background:rgba(0,255,0,0.3);border-radius:7px;color:grey;text-shadow:black 1px 1px;`
+  }else if(counTt==2){
+    document.querySelector('input[type=checkbox]').checked=false;
+    text.innerHTML='Супер блестящая фото галерея с перелистыванием по клику на фотографю. Татую галерею можно встроить в любой сайт и она будет работать везде где можно менять стили страницы и встраивать JS("Джава Скрипт" ^_^)';
+    eng.innerHTML='< - Eng';
+    styleCheckbox.innerHTML='Стили для чекбоква были взфты отсюда <a href="https://alvarotrigo.com/blog/css-checkbox-styles/">styleCheckbox</a>';
+    eng.style.cssText=`border:3px solid grey;width:100%;background:rgba(5,5,5,0.3);border-radius:7px;`
+    counTt=0;
+  }else{
+    counTt=0;
+  }
+}
+document.querySelector('body > main > span:nth-child(6)').addEventListener('click',changesTwo);
